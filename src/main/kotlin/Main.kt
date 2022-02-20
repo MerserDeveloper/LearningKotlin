05e8d0d5ad4149f7099f6f0fcba3544e431959e0
Sergey <99561610+MerserDeveloper@users.noreply.github.com>
2022-02-20 12:19:36 +0300

    Поигрался с переменными и тд

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,36 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    var x = 3
+    val name = "Vostok"
+    x = x * 10
+    println("x is $x. \n")
+    ///////////////////////////
+    while (x > 28) {
+        x = x - 1
+        println("x is now $x. \n")
+    }
+    ///////////////////////////
+    if (x == 20) {
+        println("x must be 20.")
+    } else {
+        println("x isn't be 20.")
+    }
+    if (name.equals("Vostok")) {
+        println("$name Groznyi")
+    }
+    ///////////////////////////
+    var y = 4
+    while (y > 3) {
+        println(y)
+        y = y - 1
+    }
+    ///////////////////////////
+    var k = 1
+    println("Before the loop. k = $k.")
+    while (k < 2) {
+        println("Before the loop. k = $k.")
+        k = k +1
+    }
+    println("After the loop. k = $k. \n")
+
 
 }
