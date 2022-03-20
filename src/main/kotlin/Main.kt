72283c357efcd7b84ffa73e98a91766e0a918b01
Sergey <99561610+MerserDeveloper@users.noreply.github.com>
2022-03-20 11:11:37 +0300

    Сохранил значения в массив; Cоздал мини-приложение для генерирования рекламных слоганов

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -25,12 +25,36 @@ fun main(args: Array<String>) {
     }
     ///////////////////////////
     var k = 1
+    var j = 2
     println("Before the loop. k = $k.")
     while (k < 2) {
         println("Before the loop. k = $k.")
         k = k +1
     }
     println("After the loop. k = $k. \n")
+
+    ///////////////////////////
+    //Массивы
+    ///////////////////////////
+
+    var myArrayOf = arrayOf (1,2,3)
+    println(myArrayOf[0])
+
+    val wordArray1 = arrayOf("15", "multi-terrier", "c-to-c", "dynamic","pervasive")
+    val wordArray2 = arrayOf("10", "dog", "a-to-a", "dynamic","nestle")
+    val wordArray3 = arrayOf("5", "cat", "B-to-B", "stativ","picture")
+
+    val arraySize1 = wordArray1.size
+    val arraySize2 = wordArray2.size
+    val arraySize3 = wordArray3.size
+
+    val rand1 = (Math.random() * arraySize1.toInt())
+    val rand2 = (Math.random() * arraySize2.toInt())
+    val rand3 = (Math.random() * arraySize3.toInt())
+
+    val phrase = "${wordArray1[rand1.toInt()]} ${wordArray2[rand2.toInt()]} ${wordArray3[rand3.toInt()]}"
+    println(phrase)
+    ///////////////////////////
 
 
 }
